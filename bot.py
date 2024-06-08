ceede94f29f4fb5b28f7a0ee51500a0d020d0abe
QuickSilver-1 <99834939+QuickSilver-1@users.noreply.github.com>
2024-06-08 23:42:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,12 +9,7 @@ from media import *
 from config import config_1
 from psycopg2 import connect
 from psycopg2.errors import UniqueViolation
-<<<<<<< Updated upstream
 from re import match
-=======
-from re import *
->>>>>>> Stashed changes
-
 
 dp = Dispatcher()
 bot = Bot(token=config_1.TOKEN)
